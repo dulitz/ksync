48ac9f0c76521043bc0dbbaadbc5e77100c416cd
dulitz <dulitz@gmail.com>
2021-04-09 21:12:04 -0700

    fixed install, added pull, started sync

--- ksync.py
+++ ksync.py
@@ -1,6 +1,6 @@
 #!./pyenv/bin/python
 
-import os, socket
+import heapq, math, os, socket
 import hashlib, json, subprocess, toml
 
 from datetime import datetime
@@ -42,6 +42,14 @@ def scp_toremote(user, host, localsource, remotedest, port=None):
     parg = ['-P%d' % port] if port else []
     if not printonly:
         subprocess.run(['scp'] + parg + localsources + ['%s@%s:%s' % (user, host, remotedest)], check=True)
+
+def scp_fromremote(user, host, remotesource, localdest, port=None, strictfilenames=True):
+    print('copying %s@%s:%s to %s' % (remotesource, user, host, localdest))
+    remotesources = remotesource if type(remotesource) == type([]) else [remotesource]
+    parg = ['-P%d' % port] if port else []
+    targ = [] if strictfilenames else ['-T']
+    if not printonly:
+        subprocess.run(['scp'] + parg + targ + ['%s@%s:%s' % (user, host, r) for r in remotesources] + [localdest], check=True)
 
 def ssh(user, host, cmd, tty=False, port=None, check=True):
     print('executing ssh %s@%s %s' % (user, host, cmd))
@@ -113,9 +121,10 @@ class Snapshot:
     """
     
     def __init__(self, host):
-        matches = Path('.').glob('%s-20*.ksync' % host)
+        pattern = '%s-20*.fprint' % host
+        matches = Path('.').glob(pattern)
         if not matches:
-            raise Exception('no file %s-20*.ksync in current directory' % host)
+            raise Exception('no file %s in current directory' % pattern)
         self.stack = []
         self._load(matches[-1])
 
@@ -127,39 +136,61 @@ class Snapshot:
             if based_on:
                 self._load(based_on)
 
+    def get_volumes(self):
+        return self.stack[0]['volumes'] # most recent incremental
+
+    def items(self):
+        """returns an iterator over all the (fingerprint, filelist) items"""
+        lastfp = None
+        for pair in heapq.merge(*self.stack, key=lambda p: p[0]):
+            if lastfp != pair[0]:
+                lastfp = pair[0]
+                yield pair
+                
+
 class Ksync:
     def __init__(self, config, quietmode, hostname):
         (self.config, self.quietmode) = (config, quietmode)
         self.hostname = config.get('hostmap', {}).get(hostname, hostname)
 
         if 'hosts' not in config:
-            raise Exception('config file must have hosts section')
-        for h in config['hosts']:
-            if self.hostname in h['dns']:
-                self.me = h
-                break
-        else:
-            raise Exception('config file must have an entry for %s in hosts section' % self.hostname)
+            raise Exception('config must have hosts section')
+        self.me = self._gethost(self.hostname, ifnone={})
 
-    def _get(self, variable, host=None, default=None):
+    def _gethost(self, hostname, ifnone=None):
+        for h in self.config['hosts']:
+            if hostname in h['dns']:
+                return h
+        if ifnone is None:
+            raise Exception('config must have entry for %s in hosts section' % self.hostname)
+        return ifnone
+    def _getvar(self, variable, host=None, default=None):
         if host is None:
             host = self.me
         return host.get(variable, self.config.get(variable, default))
     def _ssh_internal(self, host, cmd, tty=False, check=True):
-        ssh(self._get('scp_user', host=host), host['dns'][0], cmd, tty=tty, check=check)
+        ssh(self._getvar('scp_user', host=host), host['dns'][0], cmd, tty=tty, check=check)
     def _scp_toremote(self, host, localsources, remotedest):
-        scp_toremote(self._get('scp_user', host=host), host['dns'][0], localsources, remotedest)
+        scp_toremote(self._getvar('scp_user', host=host), host['dns'][0], localsources, remotedest)
+    def _scp_fromremote(self, host, remotesources, localdest):
+        scp_fromremote(self._getvar('scp_user', host=host), host['dns'][0], remotesources, localdest, strictfilenames=False)
 
     def install(self, host, sources):
         """ssh to the host and create its working directory if it doesn't exist. copy the config, req.txt,
         and .py files to the working directory. create venv "pyenv" and pip install -r req.txt
         """
-        workdir = self._get('working_directory', host, default='.')
+        workdir = self._getvar('working_directory', host, default='.')
         cmd = 'mkdir %s' % workdir
         self._ssh_internal(host, cmd, check=False)
         self._scp_toremote(host, sources, workdir)
         cmd = 'cd "%s" && PATH=/usr/local/bin:"$PATH" python3 -m venv pyenv && pyenv/bin/pip install --upgrade pip && pyenv/bin/pip install -r req.txt' % workdir
         self._ssh_internal(host, cmd)
+
+    def pull(self, host):
+        """scp to host to copy its fprint files here.
+        """
+        remoteworkdir = self._getvar('working_directory', host, default='.')
+        self._scp_fromremote(host, '%s/`hostname`-20*.fprint' % remoteworkdir, self._getvar('working_directory'))
 
     def _recurse_full(self, path, symlinks, fprints):
         if path.is_symlink():
@@ -169,7 +200,7 @@ class Ksync:
             s = path.stat()
             h = FileHasher(path)
             fp = h.do_hash()
-            payload = (str(path), s.st_nlink, s.st_mode, s.st_ctime, s.st_mtime)
+            payload = (str(path), s.st_size, s.st_nlink, s.st_mode, s.st_ctime, s.st_mtime)
             existing = fprints.get(fp, None)
             if existing is not None:
                 existing.append(payload)
@@ -178,8 +209,14 @@ class Ksync:
         elif path.is_dir():
             if not self.quietmode and path.parts[-1] != '@eaDir':
                 print(path)
-            for p in path.iterdir():
-                self._recurse_full(p, symlinks, fprints)
+            try:
+                for p in path.iterdir():
+                    try:
+                        self._recurse_full(p, symlinks, fprints)
+                    except PermissionError as e:
+                        print('permission denied: %s' % e)
+            except PermissionError as e:
+                print('permission denied: %s' % e)
     
     def write_full(self):
         """For each file in the filesystem, recursively, under roots specified in the config file:
@@ -199,7 +236,7 @@ class Ksync:
         fprints = {}
         for volume in volumes:
             self._recurse_full(Path(volume), symlinks, fprints)
-        workdir = self._get('working_directory', default='.')
+        workdir = self._getvar('working_directory', default='.')
         fprintpath = Path(workdir, '%s-%s-full.fprint' % (self.hostname, started.isoformat()))
         with fprintpath.open('wt') as fprint:
             fprint.write("""
@@ -209,7 +246,7 @@ class Ksync:
   "volumes": %s,
   "symlinks":
 """ % (started.isoformat(), json.dumps(volumes)))
-            json.dump(symlinks, fprint)
+            json.dump(sorted(symlinks), fprint)
             fprint.write(""",
   "fprints":
 """)
@@ -218,18 +255,73 @@ class Ksync:
 }
 """)
 
-    def write_incremental(self, base):
+    def write_incremental(self, base=None):
+        if base is None:
+            # use the most recent fprint
+            raise Exception('not implemented')
         with open(base, 'rt') as f:
             j = json.load(f)
             raise Exception('not implemented')
             
     def sync(self, source, target):
-        with open(source, 'rt') as fsource:
-            with open(target, 'rt') as ftarget:
-                jsource = json.load(fsource)
-                jtarget = json.load(ftarget)
-                raise Exception('not implemented')
-                ### TODO: write something
+        """collect every filelist in target with length > 1.
+        determine whether any directories contain only such duplicated files.
+        if the smallest number of hardlinks equals the length of the list, ...
+        otherwise ...
+
+        for every source volume that does not match a target volume, count the number of
+        files/bytes in it and print an "ignoring" line.
+
+        collect the filelist for every fprint in source that is not in target but is in
+        a source volume that matches a target volume. copy it to the first matching volume.
+        warn if pre-existing target file is newer or longer. show pre-existing versus not.
+        
+
+        given a filelist, a source volume, a target volume, and a predicate, group them 
+
+        symlinks ...
+        """
+        srchost = self._gethost(source)
+        targhost = self._gethost(target)
+        src = Snapshot(source)
+        targ = Snapshot(target)
+        targvolumes = {}
+        srcmatchprefixes = {}
+        targmatchprefixes = {}
+        for v in targ.get_volumes():
+            p = Path(v).name
+            if targvolumes.has_key(p):
+                raise Exception('target %s has duplicate volumes for %s: %s' % (target, p, repr(targ.get_volumes())))
+            targvolumes[p] = v
+        (nvolumes, nfiles, nbytes) = (0, 0, 0)
+        for v in src.get_volumes():
+            targprefix = targvolumes.get(Path(v).name, None)
+            if targprefix:
+                if srcmatchprefixes.has_key(v):
+                    raise Exception('source %s has two paths for volume %s: %s' % (source, v, repr(src.get_volumes())))
+                srcmatchprefixes[v] = targprefix
+                assert not targprefix in targmatchprefixes, (targprefix, targmatchprefixes)
+                targmatchprefixes[targprefix] = v
+            else:
+                nvolumes += 1
+        def matches(prefix, payloads):
+            for p in payloads:
+                if p[0].startswith(prefix):
+                    return True
+            return False
+        for (fprint, payloadlist) in src.items():
+            for prefix in srcmatchprefixes:
+                if matches(prefix, payloadlist):
+                    break
+            else:
+                nfiles += 1
+                nbytes += payloadlist[0][1]
+        if not self.quietmode:
+            if nfiles or nvolumes or nbytes:
+                print('ignoring %d source volumes (%d files in %d MB) not in target' % (nvolumes, nfiles, math.floor(nbytes / 1024 / 1024)))
+            else:
+                print('target includes all source volumes')
+        raise Exception('not implemented')
 
 def main(argv, quietmode=False, fullmode=False):
     if len(argv) < 3:
@@ -266,6 +358,11 @@ def main(argv, quietmode=False, fullmode=False):
         sources = [argv[2], str(Path(argv[0]).with_name('req.txt')), argv[0]] # config, req.txt, and .py files
         for h in selected:
             ksync.install(h, sources)
+    elif cmd == 'pull':
+        if not os.environ.get('SSH_AUTH_SOCK', ''):
+            raise Exception('SSH_AUTH_SOCK not set -- you should be running ssh-agent')
+        for h in selected:
+            ksync.pull(h)
     elif cmd == 'fprint':
         if len(argv) > 3:
             raise Exception('fprint runs locally only')
